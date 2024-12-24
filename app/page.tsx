3679eae25f3fc228341b9dff16e8b9da8385772f
Cody <cody.wildtyto@gmail.com>
2024-12-24 22:27:48 +0800

    feat(general): init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="card bg-base-100 w-full shadow-2xl">
+        
+      </main>
+    </div>
+  );
+}
